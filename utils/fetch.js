a23181f98d3272ba79f4926daece951d947888b6
“itNapominki” <it.napominki.mos@gmail.com>
2023-12-13 17:32:45 +0300

    Добавил обработку ошибок fetch

--- utils/fetch.js
+++ utils/fetch.js
@@ -1,36 +1,40 @@
 const CHAT_ID_SPZ = process.env.CHAT_ID_SPZ;
 
 async function updateOrdersFromFetch(postData, bot, callback) {
-  const response = await fetch(`${process.env.GOOGLE_SHEETS_KEY}`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(postData),
-  });
+  try {
+    const response = await fetch(`${process.env.GOOGLE_SHEETS_KEY}`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(postData),
+    });
 
-  let manager = await response.json();
+    let manager = await response.json();
 
-  if (response.status === 200) {
-    if (callback && typeof callback === "function") {
-      callback(null, manager.result);
-    }
+    if (response.status === 200) {
+      if (callback && typeof callback === "function") {
+        callback(null, manager.result);
+      }
 
-    return;
-  } else {
-    if (callback && typeof callback === "function") {
-      callback(
-        "Статус заявки НЕ обновлен, пожалуйста сообщите администратору группы",
-        null
+      return;
+    } else {
+      if (callback && typeof callback === "function") {
+        callback(
+          "Статус заявки НЕ обновлен, пожалуйста сообщите администратору группы",
+          null
+        );
+      }
+
+      await bot.sendMessage(
+        CHAT_ID_SPZ,
+        "Статус заявки НЕ обновлен, пожалуйста сообщите администратору группы"
       );
+
+      return;
     }
-
-    await bot.sendMessage(
-      CHAT_ID_SPZ,
-      "Статус заявки НЕ обновлен, пожалуйста сообщите администратору группы"
-    );
-
-    return;
+  } catch (error) {
+    console.error("Ошибка при выполнении запроса:", error);
   }
 }
 
